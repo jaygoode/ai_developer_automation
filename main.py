ef51d7a4fcb7a4a3dab801928d1fda72129dc52b
johnny nylund <john_newcreek@protonmail.com>
2025-05-22 22:28:26 +0300

    added Status enum and functionality, and memory functionality

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import ai_handler
 import file_handler
+from enum import Enum
+
+class Status(Enum):
+    STARTED = "STARTED"
+    BROKEN = "BROKEN"
+    DONE = "DONE"
+
+
 
 if __name__ == "__main__":
     config = file_handler.read_yaml_file("config.yaml")
@@ -17,7 +25,11 @@ if __name__ == "__main__":
     task_plan = file_handler.read_json_file(config["task_plan_json_fp"])
     for task in task_plan:
         #add to memory started id
-        
+        breakpoint()
+        if not task in file_handler.read_yaml_file(config["memory_fp"]):
+            task["status"] = Status.STARTED
+
+        file_handler.write_to_yaml_file(task, config["memory_fp"])
         #check what type of task it is - creation of file structure or code, or nothing actionable like planning
         
         #if file creation task, create a prompt requesting a terminal command for file structure build
